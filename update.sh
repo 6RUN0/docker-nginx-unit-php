200c68ec96b9a32ce470f0320edd087e410af849
boris_t <boris.t.66@gmail.com>
2022-06-22 18:35:31 +0500

    Добавил сборку контейнера для каждой версии

--- update.sh
+++ update.sh
@@ -17,11 +17,16 @@ for distr in "${distrs[@]}"; do
 	suites=( $distr/* )
 	suites=( "${suites##*/}" )
 	for suite in "$suites"; do
-		cp *.sh  "$distr/$suite/"
-		rm -f "$distr/$suite/update.sh" 
-		sed -r \
-			-e 's/%%DISTR%%/'"$distr"'/' \
-			-e 's/%%SUITE%%/'"$suite"'/' \
-			"Dockerfile.template" > "$distr/$suite/Dockerfile"
+		versions=$(wget -O - https://packages.nginx.org/unit/$distr/pool/unit/u/unit/ | sed -n -E -e "s/<a.+?>unit_(.+?)~${suite}_amd64.deb<\/a>.*/\1/p" | sort -u)
+		for ver in $versions; do
+			mkdir -p "$distr/$suite/$ver/"
+			cp *.sh  "$distr/$suite/$ver/"
+			rm -f "$distr/$suite/$ver/update.sh" 
+			sed -r \
+				-e 's/%%DISTR%%/'"$distr"'/' \
+				-e 's/%%SUITE%%/'"$suite"'/' \
+				-e 's/%%VERSION%%/'"$ver"'/' \
+				"Dockerfile.template" > "$distr/$suite/$ver/Dockerfile"
+		done
 	done
 done
