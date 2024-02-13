ec089cd1982dae35aec11ce48231638cb4f23eab
boris_t <boris.t.66@gmail.com>
2024-02-13 11:10:39 +0500

    Add php8.3

--- update.sh
+++ update.sh
@@ -12,7 +12,7 @@ if [ ${#distrs[@]} -eq 0 ]; then
 fi
 
 distrs=( "${distrs[@]%/}" )
-php_versions="7.4 8.1 8.2"
+php_versions="7.4 8.1 8.2 8.3"
 
 for distr in "${distrs[@]}"; do
 	for suite in $distr/*; do
